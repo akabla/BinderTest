8a1a6774009881263301ead8b0f2a547716ecf6b
Alexandre Kabla <ajk61@cam.ac.uk>
2021-09-16 13:43:15 +0100

    Update RHEOS.py

--- RHEOS.py
+++ RHEOS.py
@@ -2,6 +2,10 @@ from julia import Julia
 jl = Julia(compiled_modules=False)
 from julia import RHEOS
 #
-# To use, type:
-# from load_RHEOS import *
+# To use, 
+# launch python and type:
+#   from load_RHEOS import *
+# or
+# launch python using:
+#   python -i RHEOS.py
 #
